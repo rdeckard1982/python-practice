d35543841964fb18dbf07589715b5cbea689f3ab
rdeckard1982 <tran.h.nghi79@gmail.com>
2025-07-09 12:43:59 -0600

    8ball python code

---
+++ magic8ball.py
@@ -0,0 +1,29 @@
+# Write code below 💖
+import random
+
+input('Qusetion: ')
+
+num = random.randint(1, 9)
+
+match num:
+  case 1:
+    print('Yes - definitely.')
+  case 2:
+    print('It is decidedly so.')
+  case 3:
+    print('Without a doubt.')
+  case 4:
+    print('Reply hazy, try again.')
+  case 5:
+    print('Ask again letter.')
+  case 6:
+    print('Better not tell you now.')
+  case 7:
+    print('My sources say no.')
+  case 8:
+    print('Outlook not so good.')
+  case 9:
+    print('Very doubtful.')
+
+
+
